e86108485d1651eb3bfb91d7ac781465c4be8254
Leonardo Lovera <leonard.lover@gmail.com>
2023-12-13 23:56:15 -0300

    finished fm_index

--- FM_index.cpp
+++ FM_index.cpp
@@ -1,7 +1,19 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+#include <sdsl/bit_vectors.hpp>
+#include <sdsl/suffix_arrays.hpp>
+#include <sdsl/wavelet_trees.hpp>
+
 #include "FM_index.hpp"
 
-
-FM_index::FM_index(const std::vector<std::string> &docs) {
+FM_index::FM_index(const std::vector<std::string> &docs)
+{
     // Create string of concatenated docs separated by ETX
     uint64_t N, D;
     char ETX = 3;
@@ -10,21 +22,42 @@ FM_index::FM_index(const std::vector<std::string> &docs) {
     std::string text;
     for (uint64_t i = 0; i < D; i++) {
         doc_start[i] = text.length(); // Store first index of doc i in t
-        text += docs[i];
-        text += ETX;
+        text += docs[i] + ETX;
     }
-    std::ofstream archivo("temp.txt");
-    // Verificar si el archivo se abrió correctamente
-    if (archivo.is_open()) {
-        archivo << text << std::endl;
-        archivo.close();
+
+    std::ofstream t("concatenated.txt");
+    // Check if t opened correctly
+    if (t.is_open()) {
+        t << text;
+        t.close();
     } else {
-        std::cerr << "Error al abrir el archivo: " << "temp.txt" << std::endl;
+        std::cerr << "Could not open file" << std::endl;
+        std::exit(EXIT_FAILURE);
     }
-    sdsl::construct(fm_index, "temp.txt", 1);
-    std::remove("temp.txt");
+
+    sdsl::construct(fm_index, "concatenated.txt", 1);
+    std::remove("concatenated.txt");
+}
+std::vector<uint64_t> FM_index::locate(const std::string &s)
+{
+    uint64_t i, j;
+    std::unordered_set<uint64_t> doc_matches;
+    std::vector<uint64_t> matches;
+    auto occs = sdsl::locate(fm_index, s.begin(), s.end());
+
+    // Mark documents which match s
+    for (i = 0; i < occs.size(); i++) {
+        auto j = std::upper_bound(doc_start.begin(), doc_start.end(), occs[i]) - doc_start.begin();
+        doc_matches.insert(j - 1); // Minus one to get doc index
+    }
+
+    for (uint64_t d : doc_matches)
+        matches.push_back(d);
+
+    return matches;
 }
 
-int FM_index::size() {
+double FM_index::size(void)
+{
     return sdsl::size_in_mega_bytes(fm_index);
 }
